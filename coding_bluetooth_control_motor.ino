2b1e5e00a02a1af6b10b09624eb7e6eb39e2cb4d
nafzaa <78883615+nafzaa@users.noreply.github.com>
2021-03-09 12:43:59 +0800

    Create coding_bluetooth_control_motor.ino

---
+++ coding_bluetooth_control_motor.ino
@@ -0,0 +1,115 @@
+#include <SoftwareSerial.h> 
+SoftwareSerial MyBlue(10, 11); // RX | TX 
+int flag = 0; 
+int MOTOR1 = 14;
+int MOTOR2 = 15;
+int MOTOR3 = 16;
+int MOTOR4 = 17;
+
+int MOTOR5 = 18;
+int MOTOR6 = 19;
+int MOTOR7 = 20;
+int MOTOR8 = 21;
+
+int MOTOR9 = 2;
+int MOTOR10 = 3;
+
+void setup() 
+{   
+ Serial.begin(9600); 
+ MyBlue.begin(9600); 
+ pinMode(MOTOR1, OUTPUT);
+ pinMode(MOTOR2, OUTPUT);
+ pinMode(MOTOR3, OUTPUT);
+ pinMode(MOTOR4, OUTPUT);
+ pinMode(MOTOR5, OUTPUT);
+ pinMode(MOTOR6, OUTPUT);
+ pinMode(MOTOR7, OUTPUT);
+ pinMode(MOTOR8, OUTPUT);
+ pinMode(MOTOR9, OUTPUT);
+ pinMode(MOTOR10, OUTPUT);
+  
+} 
+void loop() 
+{ 
+ if (MyBlue.available()) 
+   flag = MyBlue.read(); 
+
+   if (flag == 'Z') 
+ { 
+    Stop();
+ } 
+ else if (flag == 'A') 
+ { 
+    Depan();
+ }
+ else if (flag == 'B') 
+ { 
+    Belakang();
+ }
+ else if (flag == 'C') 
+ { 
+    Kanan();
+ }
+ else if (flag == 'D') 
+ { 
+    Kiri();
+ }
+
+
+}  
+
+void Stop(){
+  digitalWrite (MOTOR1, LOW);
+  digitalWrite (MOTOR2, LOW);
+  digitalWrite (MOTOR3, LOW);
+  digitalWrite (MOTOR4, LOW);
+  digitalWrite (MOTOR5, LOW);
+  digitalWrite (MOTOR6, LOW);
+  digitalWrite (MOTOR7, LOW);
+  digitalWrite (MOTOR8, LOW);
+  }
+
+void Depan(){
+  digitalWrite (MOTOR1, LOW);
+  digitalWrite (MOTOR2, LOW);
+  digitalWrite (MOTOR3, LOW);
+  digitalWrite (MOTOR4, LOW);
+  digitalWrite (MOTOR5, LOW);
+  digitalWrite (MOTOR6, LOW);
+  digitalWrite (MOTOR7, LOW);
+  digitalWrite (MOTOR8, LOW);
+  }
+
+void Belakang(){
+  digitalWrite (MOTOR1, LOW);
+  digitalWrite (MOTOR2, LOW);
+  digitalWrite (MOTOR3, LOW);
+  digitalWrite (MOTOR4, LOW);
+  digitalWrite (MOTOR5, LOW);
+  digitalWrite (MOTOR6, LOW);
+  digitalWrite (MOTOR7, LOW);
+  digitalWrite (MOTOR8, LOW);
+  }
+
+void Kanan(){
+  digitalWrite (MOTOR1, LOW);
+  digitalWrite (MOTOR2, LOW);
+  digitalWrite (MOTOR3, LOW);
+  digitalWrite (MOTOR4, LOW);
+  digitalWrite (MOTOR5, LOW);
+  digitalWrite (MOTOR6, LOW);
+  digitalWrite (MOTOR7, LOW);
+  digitalWrite (MOTOR8, LOW);
+  }
+
+void Kiri(){
+  digitalWrite (MOTOR1, LOW);
+  digitalWrite (MOTOR2, LOW);
+  digitalWrite (MOTOR3, LOW);
+  digitalWrite (MOTOR4, LOW);
+  digitalWrite (MOTOR5, LOW);
+  digitalWrite (MOTOR6, LOW);
+  digitalWrite (MOTOR7, LOW);
+  digitalWrite (MOTOR8, LOW);
+  }
